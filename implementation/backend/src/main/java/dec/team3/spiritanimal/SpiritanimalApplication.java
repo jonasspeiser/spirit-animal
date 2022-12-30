fecde9b566c02b1373348d93a43fe3681f23181d
Jonas Speiser <a12012545@unet.univie.ac.at>
2022-12-30 12:25:05 +0100

    springinitializr

---
+++ implementation/backend/src/main/java/dec/team3/spiritanimal/SpiritanimalApplication.java
@@ -0,0 +1,13 @@
+package dec.team3.spiritanimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritanimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiritanimalApplication.class, args);
+	}
+
+}

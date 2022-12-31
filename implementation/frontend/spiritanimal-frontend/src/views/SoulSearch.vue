febb0a3b18a82c666ba23115654de902fdaa35e9
Vincent König <vincent.h.koenig@outlook.com>
2022-12-31 11:32:44 +0100

    add login-mockup in frontend

---
+++ implementation/frontend/spiritanimal-frontend/src/views/SoulSearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="soulsearch">
+    <h1>SoulSearch</h1>
+      <p>
+        To be filled with content
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SoulSearch",
+  data() {
+    return {};
+  }
+}
+</script>
+
+<style scoped>
+  #soulsearch {
+    background-color: aliceblue;
+    border: 1px solid #CCCCCC;
+    padding: 20px;
+    margin-top: 10px;
+  }
+</style>

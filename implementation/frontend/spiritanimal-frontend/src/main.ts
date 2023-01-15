4c541791217bca78691e6a50a8945b39e02d8c93
Jonas Speiser <a12012545@unet.univie.ac.at>
2023-01-15 11:27:11 +0100

    Backend: Now on SSH!

--- implementation/frontend/spiritanimal-frontend/src/main.ts
+++ implementation/frontend/spiritanimal-frontend/src/main.ts
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+// Vue.prototype.$apiUrl = 'http://backend:8080/api'
 Vue.prototype.$apiUrl = 'http://localhost:8080/api'
 
 new Vue({
